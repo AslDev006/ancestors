1adb4ec682d41f5db1253342984de33a476e5a60
AslDev006 <asldev006@gmail.com>
2024-06-04 10:55:06 +0500

    urls

--- models/urls.py
+++ models/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from .views import *
 urlpatterns = [
-    path('person/',  PeopleListView.as_view()),
+    path('people/',  PeopleListView.as_view()),
     path('roles/', PeopleListView.as_view()),
-    path('family/', FamilyListView.as_view()),
+    path('families/', FamilyListView.as_view()),
+    path('family-media/', FamilyMediaListView.as_view()),
     path('family/<id>/', FamilyDetailView),
     path('person/<id>/', PersonDetailView),
+    path('family-media/<id>/', FamilyMediaDetailView),
 ]
--- models/views.py
+++ models/views.py
@@ -30,6 +30,27 @@ class FamilyListView(APIView):
         serializer = FamilySerializer(service, many=True)
         return Response(serializer.data)
 
+class FamilyMediaListView(APIView):
+    def get(self, request, format=None):
+        service = Family_Media.objects.all()
+        serializer = FamilyMediaSerializer(service, many=True)
+        return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes((AllowAny, ))
+def FamilyMediaDetailView(request, id):
+    try:
+        user = get_object_or_404(Family_Media, id=id)
+    except Family_Media.DoesNotExist:
+        return Response(None, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = FamilyMediaSerializer(user)
+        return JsonResponse({
+            "success": True,
+            "data": serializer.data
+        }, status=200)
+
+
 @api_view(['GET'])
 @permission_classes((AllowAny, ))
 def FamilyDetailView(request, id):
