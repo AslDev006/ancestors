e0e808c0d5d93122d824bf67f725c1e64b19680e
AslDev006 <asldev006@gmail.com>
2024-06-17 14:03:46 +0500

    AdminPage

--- models/admin.py
+++ models/admin.py
@@ -6,6 +6,10 @@ class NewsAdmin(admin.ModelAdmin):
     list_filter = ['create_time', 'date_of_birth', 'first_name', 'second_name']
     date_hierarchy = 'create_time'
     search_fields = ['first_name', 'second_name', 'third_name']
-    ordering = ["create_time"]
+    ordering = ["-create_time"]
 
-admin.site.register([Roles, Family, Family_Media])
+admin.site.register([Family, Family_Media])
+
+@admin.register(Roles)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['role', 'person', 'family']
